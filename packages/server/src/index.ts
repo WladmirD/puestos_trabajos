fb683216f1c3282f47a89e4b3a9f3b0045871874
keyner07 <skerling19@hotmail.com>
2020-06-19 16:33:41 -0400

    feat: creating the basic configuration for the server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,30 @@
-console.log('works');
+
+import Express, { Application } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
+
+
+class App {
+    public app: Application;
+
+    constructor() {
+        this.app = Express();
+        this.plugins();
+    }
+
+    protected plugins(): void {
+        this.app.use(morgan('dev'));
+        this.app.use(cors());
+        this.app.use(helmet());
+        this.app.use(Express.urlencoded({ extended: false }));
+        this.app.use(Express.json());
+        this.app.use( rateLimit({ max: 100, windowMs: 30 * 60 * 1000 }));
+    }
+}
+const app = new App().app;
+const { port } = config;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
