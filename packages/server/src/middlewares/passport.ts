3562624ac582b7ad4db70808d48388a7c324e608
keyner07 <skerling19@hotmail.com>
2020-06-19 17:26:52 -0400

    feat: creating the passport strategy

---
+++ packages/server/src/middlewares/passport.ts
@@ -0,0 +1,27 @@
+import { Strategy, ExtractJwt, StrategyOptions } from 'passport-jwt';
+import { GeneralError } from '../utils/errors';
+import { User } from '../entity/user.entity';
+import { getRepository } from 'typeorm';
+
+import config from '../config/index';
+
+const opts: StrategyOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: config.jwtSecret,
+};
+
+export default new Strategy(opts, async (payload, done) => {
+    try {
+        const user = await getRepository(User);
+        const { exp } = payload;
+        if (exp < Date.now()) {
+            if (user) {
+                return done(null, user);
+            }
+            return done(null, false);
+        }
+        return done(null, false);
+    } catch (err) {
+        throw new GeneralError(err);
+    }
+});
