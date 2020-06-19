66d6ae60d57110bde7688540a92d2777652a443c
keyner07 <skerling19@hotmail.com>
2020-06-19 17:05:22 -0400

    feat: entity user

---
+++ packages/server/src/entity/user.entity.ts
@@ -0,0 +1,36 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne,
+    OneToMany, JoinColumn, CreateDateColumn, Unique
+    } from "typeorm";
+import { Job } from './job';
+import { Role } from './role';
+
+@Entity()
+@Unique(["email"])
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    roleId: number;
+    @ManyToOne(() => Role, role => role.type)
+    @JoinColumn({ name: "roleId" })
+    type: Role;
+
+    @Column()
+    url = 'str';
+
+    @CreateDateColumn()
+    created_At: Date;
+
+    @OneToMany(() => Job, job => job.owner)
+    jobs: Job[];
+}
