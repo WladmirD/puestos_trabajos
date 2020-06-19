f9b76d7fd1079c7eda7f24f1bea3f814c1f8e983
keyner07 <skerling19@hotmail.com>
2020-06-19 17:21:57 -0400

    feat: creating the folder auth

---
+++ packages/server/src/auth/index.ts
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+import { User } from '../entity/user.entity';
+import config from '../config/index';
+import { BadRequest } from '../utils/errors';
+
+export async function generateToken(id: number, role: string): Promise<string> {
+    const secretKey: string | any = config.jwtSecret;
+    return await jwt.sign({ id: id, role: role }, secretKey, {
+        expiresIn: '1h',
+    });
+}
+
+export async function comparePassword(
+    password: string,
+    hashPassword: User['password'],
+): Promise<boolean> {
+    try {
+        return await bcrypt.compare(password, hashPassword);
+    } catch (err) {
+        throw new BadRequest('Password is incorrect');
+    }
+}
+
+export async function hashedPassword(password: string): Promise<string> {
+    const saltConfig: any = config.salt;
+    const salt = await bcrypt.genSalt(parseInt(saltConfig));
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
+}
