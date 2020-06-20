c278af9098bb5a4ca6655ba8cdecbf5ac4e6da47
keyner07 <skerling19@hotmail.com>
2020-06-20 15:50:48 -0400

    feat: agregando mas tipos de errores

--- packages/server/src/utils/errors.ts
+++ packages/server/src/utils/errors.ts
@@ -11,9 +11,17 @@ export class GeneralError extends Error {
         if (this instanceof NotFound) {
             return 404;
         }
+        if (this instanceof Unauthorized) {
+            return 401;
+        }
+        if (this instanceof Forbidden) {
+            return 403;
+        }
         return 500;
     }
 }
 
 export class BadRequest extends GeneralError {}
 export class NotFound extends GeneralError {}
+export class Unauthorized extends GeneralError {}
+export class Forbidden extends GeneralError {}
