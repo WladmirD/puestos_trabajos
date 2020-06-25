479fdfd58bb6c10dc8a6220d91e971a0da21e1fd
WladmirD <wladmir.d@gmail.com>
2020-06-25 11:38:17 -0400

    Organizando Files y Folders

--- packages/frontend/src/Routes.js
+++ packages/frontend/src/Routes.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "./containers/Home";
-import NotFound from "./containers/NotFound";
-import Login from "./containers/Login";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
 
 export default function Routes() {
   return (
