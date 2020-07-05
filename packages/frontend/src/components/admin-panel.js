99399c4efc9227284c2fdd2d9c710b0da96998e2
WladmirD <wladmir.d@gmail.com>
2020-07-04 23:02:39 -0400

    Agregando Vista a Adminpanel

--- packages/frontend/src/components/admin-panel.js
+++ packages/frontend/src/components/admin-panel.js
@@ -5,7 +5,33 @@ const Admin = () => {
     return (
         <>
         <div>
+        <div >
         <h3>Admin Panel</h3>
+        </div>
+        <div className="vista" >
+        <h5>Numero Paginacion: </h5>
+        <form>
+            <input  type="text" /><button className="btn-submit1"><strong>Enviar</strong></button>
+        </form>
+        <div id="table1" >
+            <h6 className="line" ><span><strong>Tabla</strong></span></h6>
+                <table className="table table-bordered" style={{align: 'right'}}>
+                    <thead className="thead-light" >
+                        <tr>
+                            <th scope="col" ></th>
+                            <th scope="col" >Categorias</th>
+
+                        </tr>
+                    </thead>
+                   
+                </table>
+            </div>
+            </div>
+
+
+        <form>
+            <input  type="text" /><button className="btn-submit2"><strong>Enviar</strong></button>
+        </form>
         </div>
         </>
     )
