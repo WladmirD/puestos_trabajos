e30c9dde7cf11dda6dd4fb5f12347c34c8a2cd8b
keyner07 <skerling19@hotmail.com>
2020-07-06 13:31:43 -0400

    feat: edit post creado

---
+++ packages/frontend/src/pages/edit-post-page.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import Edit from '../components/edit-post';
+
+const EditPost = () => {
+    return (
+        <>
+        <Layout>
+            <Edit/>
+        </Layout>
+        </>
+    )
+}
+
+export default EditPost;
